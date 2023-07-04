347075b93ff106faa540061bd26a13b6d352a9f0
elecch <brickktania@gamil.com>
2023-07-04 14:05:57 +0900

    algorithme Stack, Queue Upload

---
+++ algorithme/Queue.js
@@ -0,0 +1,44 @@
+class Queue {
+  constructor() {
+      this.items = []
+  }
+
+  enqueue(element) {
+      this.items.push(element)
+  }
+  dequeue() {
+      return this.items.shift()
+  }
+
+  isEmpty() {
+      return this.items.length === 0
+  }
+
+  peek() {
+      if(!this.isEmpty()) {
+          return this.items[0]
+      } 
+      return null
+  }
+
+  size() {
+      return this.items.length
+  }
+
+  print() {
+      console.log(this.items.toString())
+  }
+
+}
+
+const queue = new Queue()
+console.log(queue.isEmpty())
+
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+console.log(queue.size())
+queue.print()
+
+console.log(queue.dequeue()) // 먼저 넣은게 배열의 [0]가 되어, shift()를 통해 먼저 제거된다.
+console.log(queue.peek()) // [0]이 제거된 후 배열의 다음 값인 [1]이 출력된다.
---
+++ algorithme/Stack.js
@@ -0,0 +1,43 @@
+class Stack{
+  constructor() {
+    this.items = [];
+  }
+
+  enstack(element) {
+    this.items.push(element);
+  }
+  destack(){
+    return this.items.pop()
+  }
+
+  isEmpty(){
+    return this.items.length === 0;
+  }
+
+  peek() {
+    if(!this.isEmpty()){
+      return this.items[this.items.length-1]
+    }
+    return null
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  print(){
+    console.log(this.items.toString())
+  }
+}
+
+const stack = new Stack();
+console.log(stack.isEmpty())
+
+stack.enstack(20);
+stack.enstack(30);
+stack.enstack(50);
+console.log(stack.size());
+stack.print()
+
+console.log(stack.destack());
+stack.print();
